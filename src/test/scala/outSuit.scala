5b9dde065601ca1eed49c48befad178f6ebee2de
Ashutosh Trivedi <rusty.iceberg@gmail.com>
2014-11-18 16:04:39 +0530

    testing started

--- src/test/scala/outSuit.scala
+++ src/test/scala/outSuit.scala
@@ -1,19 +1,34 @@
 /**
  * Created by ashu on 11/16/14.
  */
-import org.scalatest.FunSuite
-import org.scalatest.FlatSpec
 
-class outSuit extends FunSuite {
+import com.sun.xml.internal.ws.policy.SimpleAssertion
+import org.apache.spark.SparkContext._
+import org.apache.spark.rdd.RDD
 
-  test("An empty Set should have size 0") {
-    assert(Set.empty.size == 0)
+import org.scalatest.{BeforeAndAfterEach, FunSuite}
+
+class outSuit extends FunSuite with LocalSparkContext {
+
+
+  /*test("percentage should be less then 100") {
+    val percent = 102
+
+    val m = OutlierWithAVFModel.outliers(data,percent,sc)
+    assert(System.exit(1) == true)
   }
+*/
+  test("parameter validation") {
+       intercept[IllegalArgumentException] {
+         val v = Vector(Vector("x","y","z"), Vector("m","n"))
+         val p = sc.parallelize(v)
+         val data = p
+         val percent = 102
+         val app = OutlierWithAVFModel.outliers(data,percent,sc)
 
-  test("Invoking head on an empty Set should produce NoSuchElementException") {
-    intercept[NoSuchElementException] {
-      Set.empty.head
     }
   }
+
+
 
 }
